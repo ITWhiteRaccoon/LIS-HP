325154aabe02fe5ec301edc8a3c22f6e87cb846d
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2020-07-30 12:31:34 -0300

    Mapa de caracteres e leitura do arquivo

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,24 @@ namespace LIS_HP
 {
     class Program
     {
-        static void Main(string[] args)
+        //Criar um 'mapa'com os caracteres nas posições das chaves
+        private const string MapaChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,;!?";
+
+        //Fazer apenas uma classe devido a simplicidade
+        private static void Main(string[] args)
         {
+            Console.WriteLine(LerArquivo(args[0]));
+            Console.ReadKey();
+        }
+
+        private static string LerArquivo(string strArquivo)
+        {
+            string codigo = "";
+            using (var sr = new StreamReader(strArquivo))
+            {
+                codigo = sr.ReadToEnd();
+            }
+            return codigo;
         }
     }
 }
